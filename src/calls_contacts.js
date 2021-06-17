0cc972847cfe3ea3e3e816f370e8bc04b46a11d1
AanchalJain369 <jainaanchal369@gmail.com>
2021-06-18 01:37:04 +0530

    Alerts, editContact, deleteContact

--- src/calls_contacts.js
+++ src/calls_contacts.js
@@ -1,25 +1,30 @@
-function updateContactsList(contacts, id){
-    let contentAll="";
-    for(let i=0;i<contacts.length;i++){
-        let content="<div class='contact-card'>\
+function updateContactsList(contacts, id) {
+    let contentAll = "";
+    for (let i = 0; i < contacts.length; i++) {
+        let content = "<div class='contact-card'>\
                     <div class='upper'>\
                         <div>\
-                            <div class='avatar'>"+
-                                getAvatar(contacts[i].name)+
-                                "</div>\
-                            <span class='name'>"+
-                                contacts[i].name+
-                                "</span>\
+                            <div class='avatar'>" +
+            getAvatar(contacts[i].name) +
+            "</div>\
+                            <span class='name'>" +
+            contacts[i].name +
+            "</span>\
                         </div>\
                         <div>\
-                            <button onclick='setCallee(\""+
-                                contacts[i].name+
-                                "\")'><i class='fa fa-video-camera'></i></button>\
-                            <button><i class='fa fa-phone'></i></button>\
+                            <button onclick='setCallee(\"" +
+            contacts[i].id +
+            "\")'><i class='fa fa-video-camera'></i></button>\
+            <button onclick='editContact(\"" +
+            i +
+            "\")'><i class='fa fa-pencil'></i></button>\
+            <button onclick='deleteContact(\"" +
+            i +
+            "\")'><i class='fa fa-trash'></i></button>\
                         </div>\
                     </div>\
                 </div>";
-        contentAll+=content;
+        contentAll += content;
     }
-    document.getElementById(id).innerHTML=contentAll;
+    document.getElementById(id).innerHTML = contentAll;
 }
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
-function getAvatar(name){
-    let avatar=name[0].toUpperCase();
-    let index=name.indexOf(' ');
-    if(index!=-1) avatar+=name[index+1].toUpperCase();
+function getAvatar(name) {
+    let avatar = name[0].toUpperCase();
+    let index = name.indexOf(' ');
+    if (index != -1) avatar += name[index + 1].toUpperCase();
     return avatar;
 }
 
-function search(data, searchString){
+function search(data, searchString) {
     searchString = searchString.toLowerCase();
     for (const key in data) {
         let x = String(data[key]).toLowerCase();
@@ -14,20 +14,31 @@ function search(data, searchString){
     return false;
 }
 
-function filter(data, searchString){
-    let updates=[];
-    for(let i=0;i<data.length;i++){
-        if(search(data[i], searchString)){
+function filter(data, searchString) {
+    let updates = [];
+    for (let i = 0; i < data.length; i++) {
+        if (search(data[i], searchString)) {
             updates.push(data[i]);
         }
     }
     return updates;
 }
 
-function formToJSON(form){
-    let res={};
-    let formData=new FormData(form);
-    console.log(formData)
-    formData.forEach((val,key)=>res[key]=val);
+function formToJSON(form) {
+    let res = {};
+    let formData = new FormData(form);
+    formData.forEach((val, key) => res[key] = val);
     return res;
+}
+
+function JSONToForm(data) {
+    let formData = new FormData();
+    for (let key in data) {
+        formData.append(key, data[key]);
+    }
+    return formData;
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
 }
